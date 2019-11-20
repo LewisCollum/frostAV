e9e9042766ab88e5a2e6da2ae9152f34e0a0cf7e
ishelly404 <ishelly404>
2019-11-19 22:36:39 -0500

    fixed current display

--- source/powersupply/html/current.php
+++ source/powersupply/html/current.php
@@ -4,9 +4,20 @@
 // function 
 $output = shell_exec('(sensors | grep curr1)'); //'bash -c 'sensors');// | grep power | sed 's/[^0-9]*//g'''); 
 
+$test = trim($output);
+$test = substr($test, -2);
+
 $output = preg_replace('/^[^:]+:\s*/', '' ,$output); 
 
-$output = preg_replace('/[^0-9^.]*/', '', $output); 
+echo "$unit";
+
+$output = preg_replace('/[^0-9^.]*/', '', $output);
+
+if (strcmp($test, 'mA') !== 0) {
+	$num = floatval($output);
+	$num = $num*1000;
+	$output = strval ($num);
+}
 
 //$output2 = preg_replace( '[^0-9]*', '', $output); 
 // Display the list of all file 
