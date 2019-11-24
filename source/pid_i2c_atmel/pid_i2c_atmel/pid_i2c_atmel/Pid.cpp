dc88bb19fd4aefba58a5227ff054eb9e48f8a41c
LewisCollum <lewiscollum4@gmail.com>
2019-11-23 22:45:06 -0500

    Moved atmel related files to branch 'scrap_atmel'

--- source/pid_i2c_atmel/pid_i2c_atmel/pid_i2c_atmel/Pid.cpp
+++
@@ -1,9 +0,0 @@
-#include "Pid.hpp"
-
-int16_t Pid::updateError(int16_t newError) {
-    error.integral += newError;
-    error.derivative = newError - error.proportional;
-    error.proportional = newError;
-	int16_t temp = (gain.proportional*error.proportional + gain.integral*error.integral + gain.derivative*error.derivative) / scale;
-    return (temp);
-}
