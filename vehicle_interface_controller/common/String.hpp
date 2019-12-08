668278c5f81ef4fc301fb51e740200067087e0ea
LewisCollum <lewiscollum4@gmail.com>
2019-12-07 20:57:31 -0500

    Added TestString and added constructor for char pointers

--- vehicle_interface_controller/common/String.hpp
+++ vehicle_interface_controller/common/String.hpp
@@ -14,13 +14,24 @@ public:
     }
     
     String(): size{0}, string{0} {}
-
+    String(const char* string): size{0} {
+        char* copy = const_cast<char*>(string);
+        for (; *copy; ++size)
+            this->string[size] = *copy++;
+    }
+    
     void clear() {
         while (size != 0) string[--size] = 0;
     }
 
+    const char& operator[](int i) {
+        return string[i];
+    }
+    
+    int16_t getSize() const { return size; }
     operator char*() { return string; }
     operator const char*() { return string; }
+    operator const void*() { return string; }    
 };  
 
 #endif
---
+++ vehicle_interface_controller/common/TestString.hpp
@@ -0,0 +1,30 @@
+#include <cxxtest/TestSuite.h>
+#include "String.hpp"
+#include <iostream>
+
+class TestString: public CxxTest::TestSuite {
+public:
+    void test_volatilePointerCast() {
+        volatile char message[] = "message";
+
+        String<8> actual = const_cast<char*>(message);
+        String<8> expected = "message";
+        
+        TS_ASSERT_SAME_DATA(actual, expected, actual.getSize());
+    }
+
+    void test_assignmentOperator() {
+        String<8> actual;
+        //Test for compile error
+        actual = "message";
+    }
+
+    void test_copyCharPointer_sizeMatches() {
+        String<8> string = "FOUR";
+
+        int actual = string.getSize();
+        int expected = 4;
+
+        TS_ASSERT_EQUALS(actual, expected);
+    }
+};
