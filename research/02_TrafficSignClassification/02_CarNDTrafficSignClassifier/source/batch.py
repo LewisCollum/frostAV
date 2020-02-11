ad3b9332e6fb81eda0ae9dd36137c097292eaecd
LewisCollum <lewiscollum4@gmail.com>
2020-02-11 18:07:34 -0500

    Refactored download script, batch singleton, and added class distribution visualization

--- research/02_TrafficSignClassification/02_CarNDTrafficSignClassifier/source/batch.py
+++ research/02_TrafficSignClassification/02_CarNDTrafficSignClassifier/source/batch.py
@@ -1,4 +1,5 @@
 from keras.preprocessing.image import ImageDataGenerator
+import numpy
 
 import common
 
@@ -10,3 +11,13 @@ batch = batchGenerator.flow_from_directory(
     batch_size = size,
     shuffle = True,
     target_size = (common.imageSize, common.imageSize))
+
+images, labels = batch.next()
+
+classCount = len(labels[0])
+
+def classAt(index):
+    return numpy.where(labels[index] == 1)[0][0]
+
+sampleClasses = batch.labels
+sampleSize = batch.n
