40b61bce05674a039f0c6c0902fca0ba7b30f2cd
LewisCollum <lewiscollum4@gmail.com>
2020-03-11 13:19:33 -0400

    Added concept Node code for presentation. Also fixed cv2.imshow() not showing all windows. This problem occured because the "static" variable, totalDisplayers, in the Displayer class was not incrementing, and hence each window had the same name (the value of totalDisplayers) which means the window was overwritten on each cv2.imshow() call.

--- research/04_NodalLaneDetection/source/displayer.py
+++ research/04_NodalLaneDetection/source/displayer.py
@@ -4,8 +4,8 @@ class Displayer:
     totalDisplayers = 0
 
     def __init__(self):
-        self.id = self.totalDisplayers
-        self.totalDisplayers += 1
+        self.imageName = Displayer.totalDisplayers
+        Displayer.totalDisplayers += 1
     
     def __call__(self, frame):
-        cv2.imshow(f"{self.id}", frame)
+        cv2.imshow(f"{self.imageName}", frame)
