317ec5d382805bc4329a8871b5406453925ba18e
LewisCollum <lewiscollum4@gmail.com>
2020-05-19 02:20:32 -0400

    Added action panel

--- frostServer/static/main.js
+++ frostServer/static/main.js
@@ -24,11 +24,23 @@ function urlResponseToChartSeries(url, series) {
     })
 }
 
+let controlPanel = new cpanel.Panel("controlPanel")
+controlPanel.style = "Panel"
+controlPanel.appendToParent(document.getElementById("control"))
 
-let panel = new cpanel.ButtonPanel("panel")
-panel.buttonStyle = "PanelButton"
-panel.panelStyle = "Panel"
+let actionCategory = new cpanel.ToggleButtonCategory("Action")
+actionCategory.addButton("Drive")
+controlPanel.addCategory(actionCategory)
+controlPanel.open()
+
+
+let panel = new cpanel.Panel("panel")
+panel.style = "Panel"
 panel.appendToParent(document.getElementById("cameraContainer"))
+
+let openButton = new cpanel.PanelOpenButton(panel)
+openButton.style = "PanelOpenButton"
+openButton.appendToParent(document.getElementById("cameraContainer"))
 
 panel.addButtonChangeListener((change) => {
     var request = new XMLHttpRequest();
@@ -55,3 +67,44 @@ handleUrlResponse('/imageStreamChoices', (response) => {
         panel.addCategory(category)
     }
 })
+
+
+
+window.addEventListener("gamepadconnected", function(e) {
+    console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+                e.gamepad.index, e.gamepad.id,
+                e.gamepad.buttons.length, e.gamepad.axes.length);
+})
+
+const handleResponse = ({target}) => {
+    console.log(target.responseText)
+}
+
+var steeringPrevious = null
+var forwardPrevious = null
+var reversePrevious = null
+setInterval(function() {
+    let gamepad = navigator.getGamepads()[0]
+    if (gamepad) {
+        let steering = gamepad.axes[0]
+        let forward = gamepad.axes[4]
+        let reverse = gamepad.axes[3]
+
+        if (steering != steeringPrevious || forward != forwardPrevious || reverse != reversePrevious) {
+            const xhr = new XMLHttpRequest()
+            let update = JSON.stringify({
+                'steering': steering,
+                'forward': forward,
+                'reverse': reverse})
+            
+            xhr.addEventListener('load', handleResponse)
+            xhr.open('POST', '/gamepad')
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+            xhr.send(update)                                    
+
+            steeringPrevious = steering
+            forwardPrevious = forward
+            reversePrevious = reverse
+        }
+    }
+}, 50)
